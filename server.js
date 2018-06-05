83caf14da80345ce93520f698d4b7d4e4181672b
Bill Xu <jxphxufh@gmail.com>
2018-06-05 17:24:29 +0800

    Feat: Simple signal receiving

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-const http = require('http');
-const fs = require('fs');
-const port = 2222;
-const server = '0.0.0.0';
+const http = require('http')
+const fs = require('fs')
 
+const port = 2222
 http.createServer((request, response) => {
-  console.log(request.headers);
-  console.log(request.method);
-  console.log(request.url);
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  myReadStream.pipe(response);
-}).listen(port, server);
-console.log('Server is now running at' + server + ':' + port);
+  console.log(request.headers)
+  console.log(request.method)
+  console.log(request.statusCode)
+  console.log(request.url)
+  if (request.method == 'GET'){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(response);
+  } else if (request.method == 'POST') {
+    let buff = ''
+    request.on('data', function (chunk) {
+      buff += chunk
+      if(chunk != undefined){
+        obj = JSON.parse(chunk)
+        response.write(`\nSignal Received: ${obj.signal}`)
+      }
+    })
+    request.on('end', function () {
+      console.log(`Body: ${buff}`)
+      response.end('\nAccepted body\n\n')
+    })
+  } else {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.end('Hello World\n')
+  }
+}).listen(port)
