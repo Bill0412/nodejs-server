bba0f22f29778f4491059e2bcb4e0f8a8d6367bd
Bill Xu <jxphxufh@gmail.com>
2018-06-05 15:45:07 +0800

    Refactor: The html code is moved to index.html.
    semicolons at the end of each statement are added.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const http = require('http')
-const port = 2222
-const server = '0.0.0.0'
+const http = require('http');
+const fs = require('fs');
+const port = 2222;
+const server = '0.0.0.0';
+
 http.createServer((request, response) => {
-  console.log(request.headers)
-  console.log(request.method)
-  console.log(request.url)
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.write('<!DOCTYPE html><html><head><title>ARM book API</title></head><body><h1>Welcome here, this is where our team will flourish.</h1><a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a></body></html>')
-  response.end()
-}).listen(port, server)
+  console.log(request.headers);
+  console.log(request.method);
+  console.log(request.url);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createStream(__dirname + '/index.html', 'utf8');
+  myReadStream.pipe(res);
+}).listen(port, server);
+console.log('Server is now running at ${server}:${port}.');
